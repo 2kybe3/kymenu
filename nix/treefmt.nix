1392d4d8a4fddd07f1f1ddf5b989cde4e5fab5f0
2kybe3 <kybe@kybe.xyz>
2026-06-16 19:44:44 +0200

    treefmt: use rust-overlay

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,10 +1,14 @@
+{ pkgs, ... }:
 {
   projectRootFile = "flake.nix";
   programs = {
     taplo.enable = true;
     typos.enable = true;
     nixfmt.enable = true;
-    rustfmt.enable = true;
+    rustfmt = {
+      enable = true;
+      package = pkgs.rust-bin.stable.latest.default;
+    };
   };
   settings = {
     excludes = [
